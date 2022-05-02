9503c7f81b6fdec0341f272e2fb806d30aaa5dbe
cwh <727119026@qq.com>
2022-05-02 11:04:45 +0800

    hot-fix idea commit6

--- src/main/java/com/cwh/git/GitTest.java
+++ src/main/java/com/cwh/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
         System.out.println("Hello Git5-hot-fix!");
+        System.out.println("hot-fix06");
     }
 }
