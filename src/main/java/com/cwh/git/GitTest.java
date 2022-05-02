54ef4f4514bcfc4fc99bde181cb7d7e6aebe246e
cwh <727119026@qq.com>
2022-05-02 12:16:59 +0800

    pullAndPushTest

--- src/main/java/com/cwh/git/GitTest.java
+++ src/main/java/com/cwh/git/GitTest.java
@@ -19,6 +19,8 @@ public class GitTest {
         System.out.println("hot-fix06");
         System.out.println("push test");
         System.out.println(" test ssh");
-          System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pullAndPushTest");
+
     }
 }
